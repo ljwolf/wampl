aace8b2113eaafb3ff77de4572f231ede8780cc2
ljwolf <levi.john.wolf@gmail.com>
2016-08-22 13:08:10 -0700

    add pypi stuff

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='wes',
+      version='0.1.4',
+      description='Use Wes Anderson Color Palettes in Matplotlib & Seaborn',
+      url='https://github.com/ljwolf/wampl',
+      author='Levi John Wolf',
+      author_email='levi.john.wolf@gmail.com',
+      license='CC-BY-SA',
+      packages=['wes'],
+      install_requires=['matplotlib'],
+      zip_safe=False)
