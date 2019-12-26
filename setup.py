4188d09e8238de1be82bb991cfe728954652f83a
ljwolf <levi.john.wolf@gmail.com>
2019-12-26 21:31:02 +0000

    update package for the new include_package_data flag

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='wes',
-      version='0.1.4',
+      version='0.1.5post1',
       description='Use Wes Anderson Color Palettes in Matplotlib & Seaborn',
       url='https://github.com/ljwolf/wampl',
       author='Levi John Wolf',
@@ -9,4 +9,6 @@ setup(name='wes',
       license='CC-BY-SA',
       packages=['wes'],
       install_requires=['matplotlib'],
+      package_data ={'':'colors.json'}
+      include_package_data=True,
       zip_safe=False)
